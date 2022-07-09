db935698ed7fe741d5bf63f3116decbf9fe357bb
urlordjames <32751441+urlordjames@users.noreply.github.com>
2022-07-09 00:33:27 -0400

    add windows stub implementation of minecraft_path() so it runs

--- src/util.rs
+++ src/util.rs
@@ -1,6 +1,7 @@
 pub fn minecraft_path() -> std::path::PathBuf {
 	if cfg!(target_os = "windows") {
-		todo!("windows implementation");
+		println!("WARNING: STUB IMPLEMENTATION OF WINDOWS MINECRAFT PATH!");
+		std::path::PathBuf::from("C:/fake_dir")
 	} else if cfg!(target_os = "macos") {
 		todo!("macos implementation");
 	} else {
