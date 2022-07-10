9baf447e6a7e51f02f81731fb68115d66b629691
urlordjames <32751441+urlordjames@users.noreply.github.com>
2022-07-09 20:11:49 -0400

    add windows implementation of minecraft_path

--- src/util.rs
+++ src/util.rs
@@ -1,7 +1,10 @@
 pub fn minecraft_path() -> std::path::PathBuf {
 	if cfg!(target_os = "windows") {
-		todo!("windows implementation");
+		let appdata = std::env::var("APPDATA").unwrap();
+		let appdata_path = std::path::Path::new(&appdata);
+		appdata_path.join(".minecraft")
 	} else if cfg!(target_os = "macos") {
+		//std::path::Path::new("~/Library/Application Support/minecraft").to_path_buf()
 		todo!("macos implementation");
 	} else {
 		let home = std::env::var("HOME").unwrap();
