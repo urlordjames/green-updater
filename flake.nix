76647e441c07ed5ba00d273d17cbbeb04a395876
urlordjames <32751441+urlordjames@users.noreply.github.com>
2023-06-06 09:41:18 -0400

    add flake and cargo update

---
+++ flake.nix
@@ -0,0 +1,44 @@
+{
+	inputs = {
+		nixpkgs.url = "github:nixos/nixpkgs/release-22.11";
+		flake-utils.url = "github:numtide/flake-utils";
+
+		crane = {
+			url = "github:ipetkov/crane";
+			inputs.nixpkgs.follows = "nixpkgs";
+		};
+	};
+
+	outputs = { self, nixpkgs, flake-utils, crane }:
+		flake-utils.lib.eachDefaultSystem (system:
+			let pkgs = import nixpkgs {
+				inherit system;
+			};
+			craneLib = crane.lib.${system};
+			green-updater = craneLib.buildPackage {
+				src = craneLib.cleanCargoSource (craneLib.path ./.);
+
+				nativeBuildInputs = with pkgs; [
+					pkg-config
+					wrapGAppsHook
+				];
+
+				buildInputs = with pkgs; [ gtk4 ];
+			}; in {
+				devShell = pkgs.mkShell {
+					nativeBuildInputs = with pkgs; [
+						pkg-config
+						cargo
+					];
+
+					buildInputs = with pkgs; [ gtk4 ];
+
+					shellHook = ''
+						XDG_DATA_DIRS=$XDG_DATA_DIRS:$GSETTINGS_SCHEMAS_PATH
+					'';
+				};
+
+				packages.default = green-updater;
+			}
+		);
+}
