b5908f2fe2a750b0948b694d4b231570a8a244a4
urlordjames <32751441+urlordjames@users.noreply.github.com>
2023-06-15 19:18:30 -0400

    Sandbox -> Application

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 #![windows_subsystem = "windows"]
 
-use iced::widget::{button, column, text};
-use iced::{Alignment, Element, Sandbox, Settings};
+use iced::widget::{button, column, container, text};
+use iced::{Alignment, Application, Command, Length, Element, Settings, Theme};
 
 use std::path::PathBuf;
 
@@ -18,34 +18,46 @@ enum Message {
 	Upgrade
 }
 
-impl Sandbox for App {
+impl Application for App {
 	type Message = Message;
+	type Theme = Theme;
+	type Executor = iced::executor::Default;
+	type Flags = ();
 
-	fn new() -> Self {
-		Self {
+	fn new(_flags: ()) -> (Self, Command<Message>) {
+		(Self {
 			url: url::Url::parse("https://s3-us-east-2.amazonaws.com/le-mod-bucket/manifest.json").unwrap(),
 			mc_path: util::minecraft_path()
-		}
+		}, Command::none())
 	}
 
 	fn title(&self) -> String {
 		String::from("green updater")
 	}
 
-	fn update(&mut self, message: Message) {
+	fn update(&mut self, message: Message) -> Command<Message> {
 		match message {
 			Message::SetMCPath(path) => self.mc_path = path,
 			Message::Upgrade => todo!("upgrade")
-		}
+		};
+
+		Command::none()
 	}
 
 	fn view(&self) -> Element<Message> {
-		column![
+		let content = column![
 			text("green updater").size(50),
 			text("(licensed under GPL-3.0 or later)"),
 			text(format!("{:?}", self.mc_path)),
 			button("upgrade").on_press(Message::Upgrade),
-		].align_items(Alignment::Center).into()
+		].align_items(Alignment::Center);
+
+		container(content)
+			.width(Length::Fill)
+			.height(Length::Fill)
+			.center_x()
+			.center_y()
+			.into()
 	}
 }
 
