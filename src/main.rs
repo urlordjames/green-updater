4085740669def8f81b55d3d14c10d47b26535156
urlordjames <32751441+urlordjames@users.noreply.github.com>
2023-06-16 10:12:00 -0400

    basic upgrading

--- src/main.rs
+++ src/main.rs
@@ -9,13 +9,15 @@ use green_lib::util;
 
 struct App {
 	url: url::Url,
-	mc_path: PathBuf
+	mc_path: PathBuf,
+	can_upgrade: bool
 }
 
 #[derive(Debug, Clone)]
 enum Message {
 	SetMCPath(PathBuf),
-	Upgrade
+	Upgrade,
+	UpgradeFinished
 }
 
 impl Application for App {
@@ -27,7 +29,8 @@ impl Application for App {
 	fn new(_flags: ()) -> (Self, Command<Message>) {
 		(Self {
 			url: url::Url::parse("https://s3-us-east-2.amazonaws.com/le-mod-bucket/manifest.json").unwrap(),
-			mc_path: util::minecraft_path()
+			mc_path: util::minecraft_path(),
+			can_upgrade: true
 		}, Command::none())
 	}
 
@@ -37,19 +40,41 @@ impl Application for App {
 
 	fn update(&mut self, message: Message) -> Command<Message> {
 		match message {
-			Message::SetMCPath(path) => self.mc_path = path,
-			Message::Upgrade => todo!("upgrade")
-		};
+			Message::SetMCPath(path) => {
+				self.mc_path = path;
+				Command::none()
+			},
+			Message::Upgrade => {
+				self.can_upgrade = false;
 
-		Command::none()
+				let url = self.url.clone();
+				let mc_path = self.mc_path.clone();
+
+				Command::perform(async move {
+					let remote_dir = green_lib::Directory::from_url(url).await.unwrap();
+					remote_dir.upgrade_game_folder(&mc_path, None).await;
+				}, |_| Message::UpgradeFinished)
+			},
+			Message::UpgradeFinished => {
+				self.can_upgrade = true;
+				Command::none()
+			}
+		}
 	}
 
 	fn view(&self) -> Element<Message> {
+		let mut upgrade_button = button("upgrade");
+
+		if self.can_upgrade {
+			upgrade_button = upgrade_button.on_press(Message::Upgrade);
+		}
+
 		let content = column![
 			text("green updater").size(50),
 			text("(licensed under GPL-3.0 or later)"),
 			text(format!("{:?}", self.mc_path)),
-			button("upgrade").on_press(Message::Upgrade),
+			upgrade_button,
+			button("set to test path").on_press(Message::SetMCPath(PathBuf::from("/tmp/test")))
 		].align_items(Alignment::Center);
 
 		container(content)
