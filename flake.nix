e99baea49c4fc2f0b2a18dbf3ef730eb25a2df58
urlordjames <32751441+urlordjames@users.noreply.github.com>
2024-12-25 08:56:37 -0500

    flake update

--- flake.nix
+++ flake.nix
@@ -2,11 +2,7 @@
 	inputs = {
 		nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
 		flake-utils.url = "github:numtide/flake-utils";
-
-		crane = {
-			url = "github:ipetkov/crane";
-			inputs.nixpkgs.follows = "nixpkgs";
-		};
+		crane.url = "github:ipetkov/crane";
 	};
 
 	outputs = { self, nixpkgs, flake-utils, crane }:
